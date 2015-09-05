2565a61b1d3eebfa01e43d885c9bf0d02c864c1b
James Stone <james@jamesstone.co>
2015-09-05 09:12:13 -0700

    basic login structure wip

---
+++ source/javascripts/application-jquery.js
@@ -0,0 +1,83 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery/dist/jquery
+//= require fastclick/lib/fastclick
+//= require foundation/js/foundation
+//= require foundation/js/foundation/foundation.abide
+//= require foundation/js/foundation/foundation.accordion
+
+
+// require foundation/js/foundation/foundation.alert
+
+// move this to specific pages on blog, load on the fly
+// require foundation/js/foundation/foundation.clearing
+
+// require foundation/js/foundation/foundation.dropdown
+// require foundation/js/foundation/foundation.equalizer
+// require foundation/js/foundation/foundation.interchange
+// require foundation/js/foundation/foundation.joyride
+//= require foundation/js/foundation/foundation.magellan
+// require foundation/js/foundation/foundation.offcanvas
+//= require foundation/js/foundation/foundation.orbit
+//= require foundation/js/foundation/foundation.reveal
+// require foundation/js/foundation/foundation.slider
+// require foundation/js/foundation/foundation.tab
+// require foundation/js/foundation/foundation.tooltip
+// require foundation/js/foundation/foundation.topbar
+//= require vendor/highlight
+// require vendor/processing
+// move this to specific pages on blog, load on the fly
+//= require vendor/socialite/socialite
+//= require wookmark-jquery/jquery.wookmark.js
+//= require vendor/lazyYT
+//= require_directory .
+
+$(document).foundation();
+
+$(".card.article, .sidebar-test").click(function(){
+	window.location = $(this).find("a:first").attr("href");
+	return false;
+});
+
+// Show URL on Mouse Hover
+$(".card.article, .card.product, .sidebar-test").hover(function () {
+	window.status = $(this).find("a:first").attr("href");
+}, function () {
+	window.status = "";
+});
+
+
+// $(".front").click(function(){
+// 	$( "div.flip-container" ).toggleClass( "flip" )
+// });
+
+
+
+// ga code below
+
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+// ga('create', 'UA-19284762-1', 'jamesstone.co');
+// ga('send', 'pageview');
+
+
+$('.lazyYT').lazyYT();
+
+Socialite.load("blog-social");
+hljs.initHighlightingOnLoad();
+
+
+
--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -10,74 +10,123 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery/dist/jquery
-//= require fastclick/lib/fastclick
-//= require foundation/js/foundation
-//= require foundation/js/foundation/foundation.abide
-//= require foundation/js/foundation/foundation.accordion
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require angular/angular
+// require ui-router/release/angular-ui-router
+//= require firebase/firebase-debug
+//= require angularfire/dist/angularfire
+//= require angular-foundation/mm-foundation-tpls
+//= require angular-foundation/mm-foundation
+// require fastclick/lib/fastclick
+// require vendor/highlight
+// require vendor/socialite/socialite
+// require wookmark-jquery/jquery.wookmark.js
+// require vendor/lazyYT
+// require_directory .
 
 
-// require foundation/js/foundation/foundation.alert
+// NG APP
+var app = angular.module('memberApp', [
+	'firebase',
+	'mm.foundation',
+	]);
 
-// move this to specific pages on blog, load on the fly
-// require foundation/js/foundation/foundation.clearing
+app.controller('FormCtrl', ['$scope', '$rootScope', '$firebase', '$firebaseAuth',
+	function($scope, $firebase, $firebaseAuth) {
 
-// require foundation/js/foundation/foundation.dropdown
-// require foundation/js/foundation/foundation.equalizer
-// require foundation/js/foundation/foundation.interchange
-// require foundation/js/foundation/foundation.joyride
-//= require foundation/js/foundation/foundation.magellan
-// require foundation/js/foundation/foundation.offcanvas
-//= require foundation/js/foundation/foundation.orbit
-//= require foundation/js/foundation/foundation.reveal
-// require foundation/js/foundation/foundation.slider
-// require foundation/js/foundation/foundation.tab
-// require foundation/js/foundation/foundation.tooltip
-// require foundation/js/foundation/foundation.topbar
-//= require vendor/highlight
-// require vendor/processing
-// move this to specific pages on blog, load on the fly
-//= require vendor/socialite/socialite
-//= require wookmark-jquery/jquery.wookmark.js
-//= require vendor/lazyYT
-//= require_directory .
+        $scope.debug = false; // set to false for production
 
-$(document).foundation();
+        $scope.state = $state; // expose state to scope for nav elements
+        var fireUrl = "https://jamesstone.firebaseio.com";
+        var ref = new Firebase(fireUrl);
+        var sync = $firebase(ref);
 
-$(".card.article, .sidebar-test").click(function(){
-	window.location = $(this).find("a:first").attr("href");
-	return false;
+        $scope.authObj = $firebaseAuth(ref);
+
+        $scope.authObj.$onAuth(function(authData) {
+        	if (authData) {
+        		if ($scope.debug) console.log("authData");
+        		if ($scope.debug) console.log(authData);
+        		$scope.authData = authData;
+        	}
+        })
+
+        $scope.login = function() {
+        	if ($scope.debug) console.log("login");
+        	$scope.authObj.$authWithPassword({
+        		email: $scope.email,
+        		password: $scope.password
+        	}).then(function(authData) {
+        		if ($scope.debug) console.log(authData);
+        		$scope.authData = authData;
+        		$scope.accountError = "";
+        	}).catch(function(error) {
+            // if ($scope.debug) console.log(error);
+            if (error.toString().indexOf("password is incorrect") > -1) {
+            	$scope.accountError = "password is incorrect";
+            } else if (error.toString().indexOf("user does not exist") > -1) {
+            	$scope.accountError = "user does not exist, maybe you want to signup?";
+            } else {
+              // catchall error
+              $scope.accountError = error.toString();
+          }
+
+
+      });
+        }
+
+        $scope.resetPassword = function() {
+        	if ($scope.debug) console.log("password reset");
+        	$scope.authObj.$resetPassword({
+        		email: $scope.email
+        	}).then(function() {
+        		if ($scope.debug) console.log(authData);
+        		$scope.accountError = "password was sent to your email";
+        	}).catch(function(error) {
+        		$scope.accountError = error.toString();
+        	});
+        }
+
+        $scope.signup = function() {
+        	if ($scope.debug) console.log("login");
+        	$scope.authObj.$createUser({
+        		email: $scope.email,
+        		password: $scope.password
+        	}).then(function(authData) {
+        		if ($scope.debug) console.log(authData);
+        		$scope.authData = authData;
+        	}).catch(function(error) {
+        		if ($scope.debug) console.log(error);
+        	});
+        }
+
+        $scope.logout = function() {
+        	if ($scope.debug) console.log("logout");
+        	$scope.authObj.$unauth();
+        	$scope.authData = false;
+        }
+    }]);
+
+app.controller('MainNavCtrl', function ($scope) { 
+	// not sure what I need here
 });
 
-// Show URL on Mouse Hover
-$(".card.article, .card.product, .sidebar-test").hover(function () {
-	window.status = $(this).find("a:first").attr("href");
-}, function () {
-	window.status = "";
-});
 
 
-// $(".front").click(function(){
-// 	$( "div.flip-container" ).toggleClass( "flip" )
+// jQuery based below
+// $(document).foundation();
+// $(".card.article, .sidebar-test").click(function(){
+// 	window.location = $(this).find("a:first").attr("href");
+// 	return false;
 // });
-
-
-
-// ga code below
-
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-19284762-1', 'jamesstone.co');
-// ga('send', 'pageview');
-
-
-$('.lazyYT').lazyYT();
-
-Socialite.load("blog-social");
-hljs.initHighlightingOnLoad();
-
-
+// $(".card.article, .card.product, .sidebar-test").hover(function () {
+// 	window.status = $(this).find("a:first").attr("href");
+// }, function () {
+// 	window.status = "";
+// });
+// $('.lazyYT').lazyYT();
+// Socialite.load("blog-social");
+// hljs.initHighlightingOnLoad();
 
