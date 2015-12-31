29986b9d3a09575dfc492e2e3c862993e5b3eeea
James Stone <james@jamesstone.co>
2015-12-31 14:21:16 -0800

    added scroll

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -28,6 +28,7 @@
 // require vendor/lazyYT
 //= require moment/min/moment.min
 //= require lunr.min
+//= require angular-scroll/angular-scroll.js
 // require_directory .
 
 
@@ -37,6 +38,7 @@ var app = angular.module('memberApp', [
 	'mm.foundation',
   'oblador.lazytube',
   'autocomplete',
+  'duScroll',
   ]);
 
 app.controller('ArticlesCtrl', function($scope, $http, $window) {
@@ -76,8 +78,8 @@ app.controller('ArticlesCtrl', function($scope, $http, $window) {
 
 });
 
-app.controller('MembershipCtrl', ['$scope', '$firebase', '$firebaseAuth', '$window',
-	function($scope, $firebase, $firebaseAuth, $window) {
+app.controller('MembershipCtrl', ['$scope', '$firebase', '$firebaseAuth', '$window', '$document',
+	function($scope, $firebase, $firebaseAuth, $window, $document) {
 
 
     var ref = new Firebase("https://jamesstone.firebaseio.com/");
@@ -145,83 +147,10 @@ $scope.makeDateSortable = function(input) {
   return moment(input).format("YYYY-MM-DD");
 }
 
-/**
-    Smoothly scroll element to the given target (element.scrollTop)
-    for the given duration
-
-    Returns a promise that's fulfilled when done, or rejected if
-    interrupted
-    */
-    var smooth_scroll_to = function(element, target, duration) {
-      target = Math.round(target);
-      duration = Math.round(duration);
-      if (duration < 0) {
-        return Promise.reject("bad duration");
-      }
-      if (duration === 0) {
-        element.scrollTop = target;
-        return Promise.resolve();
-      }
-
-      var start_time = Date.now();
-      var end_time = start_time + duration;
-
-      var start_top = element.scrollTop;
-      var distance = target - start_top;
-
-    // based on http://en.wikipedia.org/wiki/Smoothstep
-    var smooth_step = function(start, end, point) {
-      if(point <= start) { return 0; }
-      if(point >= end) { return 1; }
-        var x = (point - start) / (end - start); // interpolation
-        return x*x*(3 - 2*x);
-      }
-
-      return new Promise(function(resolve, reject) {
-        // This is to keep track of where the element's scrollTop is
-        // supposed to be, based on what we're doing
-        var previous_top = element.scrollTop;
-
-        // This is like a think function from a game loop
-        var scroll_frame = function() {
-          if(element.scrollTop != previous_top) {
-            reject("interrupted");
-            return;
-          }
-
-            // set the scrollTop for this frame
-            var now = Date.now();
-            var point = smooth_step(start_time, end_time, now);
-            var frameTop = Math.round(start_top + (distance * point));
-            element.scrollTop = frameTop;
-
-            // check if we're done!
-            if(now >= end_time) {
-              resolve();
-              return;
-            }
-
-            // If we were supposed to scroll but didn't, then we
-            // probably hit the limit, so consider it done; not
-            // interrupted.
-            if(element.scrollTop === previous_top
-              && element.scrollTop !== frameTop) {
-              resolve();
-            return;
-          }
-          previous_top = element.scrollTop;
-
-            // schedule next frame for execution
-            setTimeout(scroll_frame, 0);
-          }
-
-        // boostrap the animation process
-        setTimeout(scroll_frame, 0);
-      });
-}
 
 $scope.scrollToId = function(targetId) {
-  // prep for scrolling
+  var el = angular.element(document.querySelector('#' + targetId));
+   $document.scrollToElementAnimated(el, 0, 2000);
 }
 
 
