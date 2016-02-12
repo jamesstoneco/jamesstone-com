42d0a6ed048e2334bb2e79186de399c3809ca99e
James Stone <james@jamesstone.co>
2016-02-11 20:27:55 -0800

    contact page rev

--- source/javascripts/contact.js
+++ source/javascripts/contact.js
@@ -13,7 +13,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-
+// require jquery/dist/jquery.min
+// require foundation-sites-6/dist/foundation.min
 // require_directory .
 
 
